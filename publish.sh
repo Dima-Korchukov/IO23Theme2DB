fb8995e9854080e4b577844a696bb76d1e536c4a
Dima-Korchukov <fgv38328@gmail.com>
2024-05-26 13:40:40 +0300

    update link to private repository

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/OstrykovBogdan23/IO23Theme2DB.git master:gh-pages
+git push -f https://github.com/Dima-Korchukov/IO23Theme2DB master:gh-pages
 
 cd -
